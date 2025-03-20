4883cbe2ae8b078b46e1ac82888aa4f33177b7f3
CsupityTamás <160401832+csupitytamas@users.noreply.github.com>
2025-03-20 20:53:03 +0100

    Egy static üzenetet megjelenítő componens ami jelzi nincs kivetítés.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {ValtoComponent} from './components/valto/valto.component';
+import {NoProjectionComponent} from './components/no-projection/no-projection.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   imports: [
-    ValtoComponent
+    ValtoComponent,
+    NoProjectionComponent
   ],
   styleUrls: ['./app.component.css']
 })
---
+++ frontend/src/app/components/no-projection/no-projection.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-projection',
+  templateUrl: './no-projection.component.html',
+  styleUrls: ['./no-projection.component.css']
+})
+export class NoProjectionComponent {}
