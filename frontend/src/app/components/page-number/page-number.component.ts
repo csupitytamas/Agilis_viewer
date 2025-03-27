583c1e49b0b7f2b67aa4cfc7616bf65afe035e41
CsupityTamás <160401832+csupitytamas@users.noreply.github.com>
2025-03-27 21:31:59 +0100

    Oldalszám igazítás, szerkesztés

--- frontend/src/app/components/page-number/page-number.component.ts
+++ frontend/src/app/components/page-number/page-number.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-page-number',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIcon],
   templateUrl: './page-number.component.html',
   styleUrls: ['./page-number.component.css']
 })
