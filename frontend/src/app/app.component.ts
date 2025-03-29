eced47634cefb6d5a756eb676d40cdf9aee9cae5
Tóth Benkő Balázs <h270137@stud.u-szeged.hu>
2025-03-29 16:21:10 +0100

    backend api call fix

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {NoProjectionComponent} from './components/no-projection/no-projection.component';
 import {PresentationListComponent} from './components/presentation-list/presentation-list.component';
 import {RouterOutlet} from '@angular/router';
--- frontend/src/environments/api-config.ts
+++ frontend/src/environments/api-config.ts
@@ -1,8 +1,13 @@
 const BASE_URL = window.location.origin;
+console.log('BASE_URL', BASE_URL);
 
 export const PRESENTER_API_URL = 'https://dev.backend.demosystems.hu/api/v1';
 export const SLIDEMAKER_API_URL = 'https://slidemaker-backend.onrender.com/';
 export const BACKEND_API_URL = 'http://localhost:3000';
+
+export const BACKEND_HOST_API_URL = `backend.${BASE_URL}/api`;
+console.log('BACKEND_HOST_API_URL', BACKEND_HOST_API_URL);
+
 export const BACKEND_PROXY_API_URL = `${BASE_URL}/api`;
 export const PRESENTER_PROXY_API_URL = `${BASE_URL}/api/v1`;
 export const SLIDEMAKER_PROXY_API_URL = `${BASE_URL}`;
