861a61c890f5afb89656504dbbd1d953f2c6f6ad
Tóth Benkő Balázs <h270137@stud.u-szeged.hu>
2025-03-29 16:34:45 +0100

    backend api call fix 2

--- frontend/src/environments/api-config.ts
+++ frontend/src/environments/api-config.ts
@@ -5,9 +5,6 @@ export const PRESENTER_API_URL = 'https://dev.backend.demosystems.hu/api/v1';
 export const SLIDEMAKER_API_URL = 'https://slidemaker-backend.onrender.com/';
 export const BACKEND_API_URL = 'http://localhost:3000';
 
-export const BACKEND_HOST_API_URL = `backend.${BASE_URL}/api`;
-console.log('BACKEND_HOST_API_URL', BACKEND_HOST_API_URL);
-
 export const BACKEND_PROXY_API_URL = `${BASE_URL}/api`;
 export const PRESENTER_PROXY_API_URL = `${BASE_URL}/api/v1`;
 export const SLIDEMAKER_PROXY_API_URL = `${BASE_URL}`;
