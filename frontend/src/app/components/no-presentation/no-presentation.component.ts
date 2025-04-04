d51d82d50080a0625cd6a939abcbc2549e567b71
Wolford Péter <h270824@stud.u-szeged.hu>
2025-04-04 06:22:16 +0200

    design update

--- frontend/src/app/components/no-presentation/no-presentation.component.ts
+++ frontend/src/app/components/no-presentation/no-presentation.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-no-presentation',
   imports: [],
   templateUrl: './no-presentation.component.html',
   standalone: true,
-  styleUrl: './no-presentation.component.css'
+  styleUrls: ['./no-presentation.component.css']
 })
-export class NoPresentationComponent {
+export class NoPresentationComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.exitFullscreen();
+  }
+
+  // Kilépés a fullscreen módból
+  private exitFullscreen(): void {
+    if (document.fullscreenElement) {
+      document.exitFullscreen().catch((err) => {
+        console.error("Error exiting fullscreen mode:", err);
+      });
+    }
+  }
 }
